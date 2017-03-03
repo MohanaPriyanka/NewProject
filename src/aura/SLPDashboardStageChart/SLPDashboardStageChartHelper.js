b1ea584241bd777d983ff8ddf40c61d9a21bdd5f
pyao-bwc <pyao@bluewave-capital.com>
2017-03-03 12:40:05 -0500

    Initial take at ChartJS
    Works with Locker Service turned on, no styling, and not sure the query
    for data is still correct.

--- src/aura/SLPDashboardStageChart/SLPDashboardStageChartHelper.js
+++ src/aura/SLPDashboardStageChart/SLPDashboardStageChartHelper.js
@@ -1,64 +1,40 @@
 ({
-	loadChart : function(component) {
-		google.charts.load('current', {'packages':['corechart'], 'callback': this.drawChart(component)});
-      	//google.charts.setOnLoadCallback(this.drawChart(component));
-	}, 
+    loadChart : function(component) {
+        var chartobj = component.get("v.chartobj");
+        var action = component.get("c.getAll");
+    	action.setCallback(this, function(resp) {
 
-    drawChart : function(component) {
-		var action = component.get("c.getAll");
-    	action.setCallback(this, function(resp){
-            if(resp.getState() == 'SUCCESS') {
-       			var data = new google.visualization.DataTable();
-                data.addColumn('string', resp.getReturnValue().units);
-                data.addColumn('number', 'recordCount');
-                var i;
-                for(i = 0; i < resp.getReturnValue().results.length; i++) {
-                    data.addRow([resp.getReturnValue().results[i].unit, resp.getReturnValue().results[i].recordCount]);// resp.getReturnValue().results[i].consumption
-                }
-				var formatter = new google.visualization.NumberFormat({    				
-				});
-        		formatter.format(data, 1);
-    			var options = {
-	      			hAxis: {title: resp.getReturnValue().units,  titleTextStyle: {color: '#236192'}},
-	      			vAxis: {minValue: 0},
-          			colors: ['#1c5a7d','#00AFA9','#9CB6D3','#BEC6C3','#7D98AA','#6CA6CD'],
-                    pieHole: 0.25,
-                    slices:{},
-	    		};
-                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));             
-			    chart.selectedSlice = -1;
-                function selectHandler() {
-                    var selectedItem = chart.getSelection()[0];
-                    if (selectedItem) {
-                        var value = data.getValue(selectedItem.row,0);
-                        var rowNumber = parseInt(selectedItem.row);
-                        if(chart.selectedSlice != -1){
-                            options.slices[chart.selectedSlice] = {offset:'0'};
+            var el = component.find('chart').getElement();
+            var ctx = el.getContext('2d'); 
+
+            //if chartobj is not empty, then destory the chart in the view
+            if(chartobj){
+                chartobj.destroy();
+            }
+
+            console.log('resp labels: ' + resp.getReturnValue().labels);
+            console.log('resp data: ' + resp.getReturnValue().data);
+
+            chartobj = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: resp.getReturnValue().labels,
+                    datasets: [
+                        {
+                            data: resp.getReturnValue().counts
                         }
-                        if(chart.selectedSlice == rowNumber){
-                            chart.selectedSlice = -1;
-                            var value = "All";
-                            var evt = $A.get("e.c:SLPStageChartEvent");
-                            evt.setParams({"stageName": value});
-                            evt.fire();                               
-                        }else{
-                            options.slices[rowNumber] = {offset:'.2'};
-                            chart.selectedSlice = rowNumber;
-                            var evt = $A.get("e.c:SLPStageChartEvent");
-                            evt.setParams({"stageName": value});
-                            evt.fire();                            
-                        }
-                        chart.draw(data,options);
+                    ]
+                },
+                options: {
+                    hover: {
+                        mode: "none"
                     }
-                }		        
-                google.visualization.events.addListener(chart,'select', selectHandler);
-                chart.draw(data, options);
-                
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-    	});
-	    $A.enqueueAction(action);
-	}
+                }
+            });
+
+            //store the chart in the attribute
+            component.set("v.chartobj",chartobj);
+        });
+        $A.enqueueAction(action);
+    }
 })
