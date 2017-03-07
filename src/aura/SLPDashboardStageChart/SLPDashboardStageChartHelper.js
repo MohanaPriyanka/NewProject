12a4aa8ed6e5736a2e06e4485918a33a0ab5d2c6
pyao-bwc <pyao@bluewave-capital.com>
2017-03-07 08:36:50 -0500

    Add padding and event handling

--- src/aura/SLPDashboardStageChart/SLPDashboardStageChartHelper.js
+++ src/aura/SLPDashboardStageChart/SLPDashboardStageChartHelper.js
@@ -3,9 +3,8 @@
         var chartobj = component.get("v.chartobj");
         var action = component.get("c.getAll");
     	action.setCallback(this, function(resp) {
-
-            var el = component.find('chart').getElement();
-            var ctx = el.getContext('2d'); 
+            var canvas = component.find('chart').getElement();
+            var ctx = canvas.getContext('2d'); 
 
             //if chartobj is not empty, then destory the chart in the view
             if(chartobj){
@@ -20,16 +19,10 @@
                         {
                             data: resp.getReturnValue().counts,
                             backgroundColor: ['#1c5a7d','#00AFA9','#9CB6D3','#BEC6C3','#7D98AA','#6CA6CD'],
-                            hoverBorderWidth: [10, 10, 10, 10, 10, 10, 10]
+                            hoverBorderWidth: [8, 8, 8, 8, 8, 8, 8]
                         }]
                 },
                 options: {
-                    title: {
-                        display: true,
-                        fontSize: 20,
-                        padding: 30,
-                        text: 'Pending Tasks by Stage'
-                    },
                     legend: {
                         display: true,
                         position: 'right'
@@ -39,6 +32,31 @@
                     }
                 }
             });
+
+            canvas.onclick = function(evt) {
+                var activePoints = chartobj.getElementsAtEvent(evt);
+                var sce = $A.get("e.c:SLPStageChartEvent");
+                var stageName;
+                try {
+                    var chartData = activePoints[0]['_chart'].config.data;
+                    var idx = activePoints[0]['_index'];
+                    var label = chartData.labels[idx];
+                    var value = chartData.datasets[0].data[idx];
+                    stageName = label;
+                } catch (err) {
+                    // Get this: Uncaught TypeError: Cannot read property '_chart' of undefined
+                    // When clicking off the chart. There's probably a better way of handling
+                    // this?
+                    if (err.name === 'TypeError') {
+                        stageName = "All";
+                    } else {
+                        throw err;
+                    }
+                }
+                console.log("firing SLPStageChartEvent, stageName: " + stageName);
+                sce.setParams({"stageName": stageName});
+                sce.fire();
+            }
 
             //store the chart in the attribute
             component.set("v.chartobj",chartobj);
