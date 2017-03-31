4ede8142cc2e0c1f3a5a0f6f7186645dc5158430
pyao-bwc <pyao@bluewave-capital.com>
2017-03-31 10:43:42 -0400

    Credit Pull Status Poller

--- src/aura/SLPAddCustomer/SLPAddCustomerController.js
+++ src/aura/SLPAddCustomer/SLPAddCustomerController.js
@@ -13,9 +13,8 @@
     },    
 
     addCustomer : function(component, event, helper) {
-        var submitButton = component.find("SubmitButton");
-        $A.util.addClass(submitButton, 'noDisplay'); 
-
+        $A.util.addClass(component.find("SubmitButton"), 'noDisplay'); 
+        // $A.util.removeClass(component.find("leadSpinner"), 'slds-hide');
         var spinner = component.find("leadSpinner");
         var evt = spinner.get("e.toggle");
         evt.setParams({ isVisible : true });
@@ -55,6 +54,7 @@
                         $A.util.removeClass(pullCreditButtons, 'noDisplayBar');
                         $A.util.removeClass(addedCustomerConfirmCredit, 'noDisplayBar');
                     } else {
+                        // $A.util.addClass(component.find("leadSpinner"), 'slds-hide'); 
                         var spinner2 = component.find('leadSpinner');
                         var evt2 = spinner.get("e.toggle");
                         evt2.setParams({ isVisible : false });
@@ -76,6 +76,7 @@
             alert("Please acknowledge our privacy policy, give BlueWave permission " +
                   "to access credit history, energy history and fill out all of the fields on this form.");
 
+            // $A.util.addClass(component.find("leadSpinner"), 'slds-hide'); 
             var spinner2 = component.find('leadSpinner');
             var evt2 = spinner.get("e.toggle");
             evt2.setParams({ isVisible : false });
@@ -87,9 +88,8 @@
     },
     
     checkCredit : function(component, event, helper) {
-        var submitButton = component.find("pullCreditButtons");
-        $A.util.addClass(submitButton, 'noDisplay'); 
-
+        $A.util.addClass(component.find("pullCreditButtons"), 'noDisplay'); 
+        // $A.util.removeClass(component.find("creditSpinner"), 'slds-hide');
         var spinner = component.find("creditSpinner");
         var evt = spinner.get("e.toggle");
         evt.setParams({ isVisible : true });
@@ -97,18 +97,37 @@
 
         var lead = component.get("v.newLead");
         if (!$A.util.isUndefinedOrNull(lead.Id)) {
-            alert("calling pullCreditStatus");
+            var self = this;
             var action = component.get("c.pullCreditStatus");
             action.setParams({"lead" : lead});
             action.setCallback(this, function(resp) {
                     if(resp.getState() == "SUCCESS") {
-                        alert("checkbox checked");
-                        var addedCustomerConfirmCredit = component.find("addedCustomerConfirmCredit");
-                        var pullCreditButtons = component.find("pullCreditButtons");
+                        // Checkbox checked
+                        window.setTimeout(function() {
+                                $A.util.removeClass(component.find("creditStatus"), 'noDisplay');
+                                component.set("v.creditStatusText", "Sending request to TransUnion");
+                            }, 2000);
+                        window.setTimeout(function() {
+                                component.set("v.creditStatusText", "Waiting for TransUnion to process...");
+                            }, 4000);
+                        window.setTimeout(function() {
+                                component.set("v.creditStatusText", "Checking for results...");
+                            }, 7000);
+                        window.setTimeout(function() {
+                                var creditPollerInterval = window.setInterval(helper.checkCreditStatus, 2000, component);
+                                component.set("v.creditStatusPoller", creditPollerInterval);
+                            }, 9000);
+                        // checkCreditStatus should clearInterval if it finds a Credit Report Log or
+                        // a Credit Report on the Lead, but just in case, stop checking after a minute
+                        window.setTimeout(function() {
+                                var evt2 = component.find('creditSpinner').get("e.toggle");
+                                evt2.setParams({ isVisible : false });
+                                evt2.fire();
 
-                        $A.util.addClass(addedCustomerConfirmCredit, 'noDisplayBar');
-                        $A.util.addClass(pullCreditButtons, 'noDisplayBar');
+                                window.clearInterval(component.get("v.creditStatusPoller"));
+                            }, 60000);
                     } else {
+                        // $A.util.addClass(component.find("creditSpinner"), 'slds-hide');
                         var spinner2 = component.find('creditSpinner');
                         var evt2 = spinner.get("e.toggle");
                         evt2.setParams({ isVisible : false });
@@ -119,7 +138,7 @@
 
                         var appEvent = $A.get("e.c:ApexCallbackError");
                         appEvent.setParams({"className" : "SLPAddCustomerController",
-                                            "methodName" : "pullCreditStatus",
+                                            "methodName" : "checkCredit",
                                             "errors" : resp.getError()});
                         appEvent.fire();
                         $A.log("Errors", resp.getError());
