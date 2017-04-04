4cf8b7118e0153d091812834bb12bd5f27ff9030
pyao-bwc <pyao@bluewave-capital.com>
2017-04-04 14:18:11 -0400

    Add noDisplay to Disbursals by default
    To get the navigation bar rendering as expected.

--- src/aura/SLPNavigationBar/SLPNavigationBarController.js
+++ src/aura/SLPNavigationBar/SLPNavigationBarController.js
@@ -5,8 +5,10 @@
         actionLicenseType.setCallback(this,function(resp){
             if(resp.getState() == 'SUCCESS') {
                 if(resp.getReturnValue().length > 0){
-                    if(resp.getReturnValue() == 'Executive')
-                    component.set("v.licenseType", true);
+                    if(resp.getReturnValue() == 'Executive') {
+                        component.set("v.licenseType", true);
+                        $A.util.removeClass(component.find("disbursalsMenuItem"), 'noDisplay');
+                    }
                 }
             }    
             else {
