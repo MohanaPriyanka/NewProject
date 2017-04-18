b052cb96a96adf1dc6ed08dbe70aac499d8f3d24
cmswain <cswain@bluewave-capital.com>
2017-04-18 10:49:30 -0400

    Test classa adjustments
    removed the alerts from the UserHelper

--- src/aura/SLPLandingPage/SLPLandingPageController.js
+++ src/aura/SLPLandingPage/SLPLandingPageController.js
@@ -1,20 +1,20 @@
 ({
 	doInit : function(component, event, helper) {
         
-        var actionAlerts = component.get("c.getPartnerAlerts");        
-        actionAlerts.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                var alertTheme = component.find("partnerAlertTheme");                   
-                if(resp.getReturnValue().length > 0){
-                    component.set("v.partnerAlerts", resp.getReturnValue());
-                    component.set("v.alertPrompt", true);
-                }
-            }    
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });    
-        $A.enqueueAction(actionAlerts);   
+        // var actionAlerts = component.get("c.getPartnerAlerts");        
+        // actionAlerts.setCallback(this,function(resp){
+        //     if(resp.getState() == 'SUCCESS') {
+        //         var alertTheme = component.find("partnerAlertTheme");                   
+        //         if(resp.getReturnValue().length > 0){
+        //             component.set("v.partnerAlerts", resp.getReturnValue());
+        //             component.set("v.alertPrompt", true);
+        //         }
+        //     }    
+        //     else {
+        //         $A.log("Errors", resp.getError());
+        //     }
+        // });    
+        // $A.enqueueAction(actionAlerts);   
         
         var actionLogo = component.get("c.getPartnerLogo");        
         actionLogo.setCallback(this,function(resp){
@@ -31,45 +31,45 @@
         
 	},
 
-    acknowledgeAlert : function(component, event, helper) { 
-        var partnerAlertId = component.get("v.partnerAlerts[0].Id");
-        var actionAcknowledgeAlert = component.get("c.acknowledgePartnerAlert");
+    // acknowledgeAlert : function(component, event, helper) { 
+    //     var partnerAlertId = component.get("v.partnerAlerts[0].Id");
+    //     var actionAcknowledgeAlert = component.get("c.acknowledgePartnerAlert");
         
-        actionAcknowledgeAlert.setParams({partnerAlertId : partnerAlertId});
-        actionAcknowledgeAlert.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                var alert = component.find("partnerAlert");    
-                $A.util.addClass(alert, 'noDisplay');            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });            
-        $A.enqueueAction(actionAcknowledgeAlert); 
+    //     actionAcknowledgeAlert.setParams({partnerAlertId : partnerAlertId});
+    //     actionAcknowledgeAlert.setCallback(this,function(resp){
+    //         if(resp.getState() == 'SUCCESS') {
+    //             var alert = component.find("partnerAlert");    
+    //             $A.util.addClass(alert, 'noDisplay');            }
+    //         else {
+    //             $A.log("Errors", resp.getError());
+    //         }
+    //     });            
+    //     $A.enqueueAction(actionAcknowledgeAlert); 
 
-        $A.get('e.force:refreshView').fire();          
-    },     
+    //     $A.get('e.force:refreshView').fire();          
+    // },     
 
-    closeAlert : function(component, event, helper) {
+    // closeAlert : function(component, event, helper) {
 
-        var partnerAlertId = component.get("v.partnerAlerts[0].Id");
-        var actionRemindLater = component.get("c.partnerAlertRemindLater");
+    //     var partnerAlertId = component.get("v.partnerAlerts[0].Id");
+    //     var actionRemindLater = component.get("c.partnerAlertRemindLater");
         
-        actionRemindLater.setParams({partnerAlertId : partnerAlertId});
-        actionRemindLater.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                var alert = component.find("partnerAlert");    
-                $A.util.addClass(alert, 'noDisplay');            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });            
-        $A.enqueueAction(actionRemindLater); 
+    //     actionRemindLater.setParams({partnerAlertId : partnerAlertId});
+    //     actionRemindLater.setCallback(this,function(resp){
+    //         if(resp.getState() == 'SUCCESS') {
+    //             var alert = component.find("partnerAlert");    
+    //             $A.util.addClass(alert, 'noDisplay');            }
+    //         else {
+    //             $A.log("Errors", resp.getError());
+    //         }
+    //     });            
+    //     $A.enqueueAction(actionRemindLater); 
 
 
-        var alert = component.find("partnerAlert");    
-        $A.util.addClass(alert, 'noDisplay');   
-        $A.get('e.force:refreshView').fire();          
-    },         
+    //     var alert = component.find("partnerAlert");    
+    //     $A.util.addClass(alert, 'noDisplay');   
+    //     $A.get('e.force:refreshView').fire();          
+    // },         
     
     removeDisplay : function(component, event, helper) {
     	var widgetsToggle = component.find("disbursalWidgets");
