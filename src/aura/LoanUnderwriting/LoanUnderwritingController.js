324db02beca969e42cec984f22abd398e9d5f009
pyao-bwc <peteryao@PETERs-MacBook-Pro.local>
2017-06-12 05:46:20 -0400

    Start to hook up to apex
    Unfortunately, Lightning Data Service doesn’t work with lightning out
    yet.

--- src/aura/LoanUnderwriting/LoanUnderwritingController.js
+++ src/aura/LoanUnderwriting/LoanUnderwritingController.js
@@ -1,4 +1,19 @@
 ({
-	myAction : function(component, event, helper) {
-	}
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getLoanInfo");
+        action.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.loanInfo", resp.getReturnValue());
+            } else {
+                $A.log("Errors", resp.getError());
+                var appEvent = $A.get("e.c:ApexCallbackError");
+                appEvent.setParams({"className" : "LoanUnderwritingController",
+                    "methodName" : "doInit",
+                    "errors" : resp.getError()});
+                appEvent.fire();
+                $A.log("Errors", resp.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    }
 })
