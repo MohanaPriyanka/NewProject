8df94c065200d66ed38f26c75f9a5dd1f7251bac
cmswain <cswain@bluewave-capital.com>
2017-06-13 16:00:52 -0400

    Commit

--- src/aura/CSCPSolarGenerationChart/CSCPSolarGenerationChartHelper.js
+++ src/aura/CSCPSolarGenerationChart/CSCPSolarGenerationChartHelper.js
@@ -1,7 +1,7 @@
 ({
     loadChart : function(component) {
         var chartobj = component.get("v.chartobj");
-        var action = component.get("c.getAll");
+        var action = component.get("c.getGenerationSavingsData");
         action.setCallback(this, function(resp) {
             var canvas = component.find('chart').getElement();
             var ctx = canvas.getContext('2d'); 
@@ -12,19 +12,49 @@
             }
             
             chartobj = new Chart(ctx, {
-                type: 'pie',
+                type: 'bar',
                 data: {
-                    labels: resp.getReturnValue().labels,
+                    labels: resp.getReturnValue().yearMonth,
                     datasets: [
+                        {   type: 'bar',
+                            data: resp.getReturnValue().savings,
+                            backgroundColor: '#7D98AA',
+                            hoverBorderWidth: 8,
+                            stack: 1,
+                            label: 'Savings This Month'
+                        },
                         {
-                            data: resp.getReturnValue().counts,
-                            backgroundColor: ['#1c5a7d','#00AFA9','#9CB6D3','#BEC6C3','#7D98AA','#6CA6CD', '#40637B', '#E8F2F9'],
-                            hoverBorderWidth: [8, 8, 8, 8, 8, 8, 8]
-                        }]
-                },
+                            type: 'bar',
+                            data: resp.getReturnValue().totalSavings,
+                            backgroundColor: '#1c5a7d',
+                            hoverBorderWidth: 8,
+                            stack: 1,
+                            label: 'Total Savings'
+                        },                        
+                    ]
+                },                
                 options: {
+                    title: {
+                        display: true,
+                        text: 'Community Solar Savings'
+                    },                    
+                    scales: {
+                        yAxes: [{
+                            stacked: true,
+                            ticks: {
+                                beginAtZero: true,
+                                callback: function(value, index, values) {
+                                    return '$' + parseFloat(Math.round(value * 100) / 100).toFixed(2);;
+                                }
+                            }
+                        }],
+                        xAxes: [{
+                            stacked: true,
+                        }]                        
+                    },                    
                     legend: {
                         display: true,
+                        labels: ['Savings this month', 'Total Savings'],
                         position: 'right',
                         // By default, clicking on a legend item filters the chart. We want to enable
                         // this only when we can also fire the SLPStageChartEvent
@@ -36,30 +66,30 @@
                 }
             });
 
-            canvas.onclick = function(evt) {
-                var activePoints = chartobj.getElementsAtEvent(evt);
-                var sce = $A.get("e.c:SLPStageChartEvent");
-                var stageName;
-                try {
-                    var chartData = activePoints[0]['_chart'].config.data;
-                    var idx = activePoints[0]['_index'];
-                    var label = chartData.labels[idx];
-                    var value = chartData.datasets[0].data[idx];
-                    stageName = label;
-                } catch (err) {
-                    // Get this: Uncaught TypeError: Cannot read property '_chart' of undefined
-                    // When clicking off the chart. There's probably a better way of handling
-                    // this?
-                    if (err.name === 'TypeError') {
-                        stageName = "All";
-                    } else {
-                        throw err;
-                    }
-                }
-                console.log("firing SLPStageChartEvent, stageName: " + stageName);
-                sce.setParams({"stageName": stageName});
-                sce.fire();
-            }
+            // canvas.onclick = function(evt) {
+            //     var activePoints = chartobj.getElementsAtEvent(evt);
+            //     var sce = $A.get("e.c:SLPStageChartEvent");
+            //     var stageName;
+            //     try {
+            //         var chartData = activePoints[0]['_chart'].config.data;
+            //         var idx = activePoints[0]['_index'];
+            //         var label = chartData.labels[idx];
+            //         var value = chartData.datasets[0].data[idx];
+            //         stageName = label;
+            //     } catch (err) {
+            //         // Get this: Uncaught TypeError: Cannot read property '_chart' of undefined
+            //         // When clicking off the chart. There's probably a better way of handling
+            //         // this?
+            //         if (err.name === 'TypeError') {
+            //             stageName = "All";
+            //         } else {
+            //             throw err;
+            //         }
+            //     }
+            //     console.log("firing SLPStageChartEvent, stageName: " + stageName);
+            //     sce.setParams({"stageName": stageName});
+            //     sce.fire();
+            // }
 
             // store the chart in the attribute
             component.set("v.chartobj",chartobj);
