418d11903a0d05cf9b45a5ec3924aaed17d6e71c
jpentaleri <jpentaleri@bluewave-capital.com>
2017-06-21 16:59:53 -0400

    Make a Preview Window
    Instead of just generating the document, now there is a second button
    "View Loan Data" that shows the calculated monthly payment etc. in a
    table before generating the document in Conga.

--- src/aura/SLPQuickSalesSheetController/SLPQuickSalesSheetControllerController.js
+++ src/aura/SLPQuickSalesSheetController/SLPQuickSalesSheetControllerController.js
@@ -1,15 +1,24 @@
 ({   
     clickCreateQSS : function(component, event, helper) {
-        var newSheet = component.get("v.quicksalessheet");        
+        var newSheet = component.get("v.quicksalessheet"); 
+        var btnClicked = event.getSource().get("v.name");
+		var genDoc = false;
+        if (btnClicked == 'submitUpfront') {
+            genDoc = true;
+        } else if (btnClicked == 'refreshbutton'){
+            $A.util.addClass(component.find("loaninfocard"), 'noDisplay');
+        }
         var actionAddNewQss = component.get("c.addNewQSS");
         
         actionAddNewQss.setParams({
-            "newQSS": newSheet
+            "newQSS": newSheet, 
+            "generateDoc": genDoc
         });
         
         actionAddNewQss.setCallback(this,function(resp) {		
-            if(resp.getState() == "SUCCESS") {                
+            if(resp.getState() == "SUCCESS" && genDoc) {                
                 $A.util.addClass(component.find("submitQSSbutton"), 'noDisplay');
+               	$A.util.addClass(component.find("viewLoanData"), 'noDisplay');
                 $A.util.removeClass(component.find("spinnerandtext"), 'noDisplay');
                                               
                     window.setTimeout(function() {
@@ -36,6 +45,20 @@
                         }
                      	}, 30000);
             }
+            else if(resp.getState() == "SUCCESS" && !genDoc) { 
+                $A.util.addClass(component.find("submitQSSbutton"), 'noDisplay');
+                $A.util.addClass(component.find("viewLoanData"), 'noDisplay');
+                $A.util.removeClass(component.find("spinnerandtext"), 'noDisplay');
+
+                window.setTimeout(function() {
+                    var qssIdVar = resp.getReturnValue().Id;
+                	helper.getQSS(component,qssIdVar);
+               		$A.util.addClass(component.find("spinnerandtext"), 'noDisplay');
+                	$A.util.removeClass(component.find("loaninfocard"), 'noDisplay');
+                    $A.util.removeClass(component.find("documentCreatebutton"), 'noDisplay');
+                }, 700);
+                
+            }
         });
         
         $A.enqueueAction(actionAddNewQss);
@@ -48,11 +71,53 @@
                 component.set("v.ProductListAttribute", resp.getReturnValue());    
             } else {
             }
-        });    
+        });   
         $A.enqueueAction(actionProducts);
         $A.util.addClass(component.find("downloadQSSbutton"), 'noDisplay');
         $A.util.addClass(component.find("downloadQSStext"), 'noDisplay');
         $A.util.addClass(component.find("spinnerandtext"), 'noDisplay');
+        $A.util.addClass(component.find("loaninfocard"), 'noDisplay');
     },    
+    
+    clickCreateDocument : function(component, event, helper) {
+        var qssToUpdate = component.get("v.calculatedQSS");
+        var qssIdVarible = component.get("v.calculatedQSS.Id");
+        var actionCreateDocQss = component.get("c.updateQSS");
+        
+        actionCreateDocQss.setParams({
+            "updatedQSS" : qssToUpdate, 
+        });
+        
+        actionCreateDocQss.setCallback(this,function(resp) {
+            $A.util.removeClass(component.find("spinnerandtext"), 'noDisplay');
+            $A.util.addClass(component.find("documentCreatebutton"), 'noDisplay');
+            $A.util.addClass(component.find("loaninfocard"), 'noDisplay');
+       
+            window.setTimeout(function() {
+                    $A.util.removeClass(component.find("docStatus"), 'noDisplay');    
+                    component.set("v.docStatusText", "Creating the Document...");
+                    }, 6000);
+               
+            window.setTimeout(function() {
+                    component.set("v.docStatusText", "Generating a download link...");
+                    }, 8000);
+               
+            window.setTimeout(function() {
+                    var docInterval = window.setInterval($A.getCallback(function() {helper.getDocument(component,qssIdVarible);}), 2000);
+                       component.set("v.docStatusPoller", docInterval);
+                    }, 12000);  
+               
+            window.setTimeout(function() {
+                    window.clearInterval(component.get("v.docStatusPoller"));
+                    var successLink = component.get("v.qssLinkToFile");
+                    if (successLink == "notgeneratedinportal") {
+                        component.set("v.docStatusText", "An Error Occurred While Creating Your Document, Please Contact BlueWave at partnersupport@bluewavesolar.com");
+                        $A.util.addClass(component.find("qssSpinner"), 'noDisplay');
+                   }
+                   }, 30000);
+        });
+        
+        $A.enqueueAction(actionCreateDocQss);
+    },   
 
 })
