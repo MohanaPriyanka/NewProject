509d91891c37d1858a19f3ef5466130be6f54265
cmswain <cswain@bluewave-capital.com>
2017-07-17 10:29:15 -0400

    Changes after PYAO Comments 2

--- src/aura/SLPCreditStatus/SLPCreditStatusHelper.js
+++ src/aura/SLPCreditStatus/SLPCreditStatusHelper.js
@@ -6,10 +6,6 @@
             leadId = component.get("v.leadId");
         }
         var action = component.get("c.getCustomerProducts"); 
-        var customerTable = component.find("customerTable");
-        var productTable = component.find("productTable");
-        var searchBar = component.find("customerSearchBar");
-        var searchButton = component.find("customerSearchButton");
 
         component.set("v.leadId", leadId);
 
@@ -17,11 +13,14 @@
 
         action.setCallback(this,function(resp){ 
             if(resp.getState() == 'SUCCESS') {
+                if (resp.getReturnValue().length == 0) {
+                    component.set("v.noAvailableProducts", true);                    
+                }
                 component.set("v.allProducts", resp.getReturnValue());
-                $A.util.addClass(customerTable, 'noDisplay');
-                $A.util.addClass(searchButton, 'noDisplay');
-                $A.util.addClass(searchBar, 'noDisplay');
-                $A.util.removeClass(productTable, 'noDisplay');
+                $A.util.addClass(component.find("customerTable"), 'noDisplay');
+                $A.util.addClass(component.find("customerSearchButton"), 'noDisplay');
+                $A.util.addClass(component.find("customerSearchBar"), 'noDisplay');
+                $A.util.removeClass(component.find("productTable"), 'noDisplay');
             }
             else {
                 $A.log("Errors", resp.getError());
