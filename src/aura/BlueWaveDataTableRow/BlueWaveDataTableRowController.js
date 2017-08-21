f7ce3b5ba8dae5b51403236e8280e06dc91235be
cmswain <cswain@bluewave-capital.com>
2017-08-21 13:49:23 -0400

    Customer Compression Commit 1

---
+++ src/aura/BlueWaveDataTableRow/BlueWaveDataTableRowController.js
@@ -0,0 +1,11 @@
+({
+    doInit : function(component, helper) {
+        var record = component.get('v.objectRecord');
+        var fieldName = component.get('v.fieldName');
+        var outputText = component.find("outputTextId");
+
+        if(record[fieldName] != null) {
+        	outputText.set("v.value", record['Product__r'][fieldName]);
+        }
+    }
+})
--- src/aura/SLPAllCustomers/SLPAllCustomersController.js
+++ src/aura/SLPAllCustomers/SLPAllCustomersController.js
@@ -24,8 +24,9 @@
                 $A.log("Errors", resp.getError());
             }
         });        
-        $A.enqueueAction(action);     
-              
+        $A.enqueueAction(action);  
+        component.set("v.sortAsc", true);   
+        component.set("v.sortField", "Name");   
 	},
     
     searchCustomers : function(component, event, helper) {            
@@ -46,6 +47,38 @@
         $A.enqueueAction(action);        
     },
     
+    searchCustomers : function(component, event, helper) {            
+        var input = component.find("customerSearch");
+        var customerName = input.get("v.value");           
+        var action = component.get("c.getAllCustomers");     
+        
+        action.setParams({searchValue : customerName});
+        
+        action.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.allCustomers", resp.getReturnValue());
+            }
+            else {
+                $A.log("Errors", resp.getError());
+            }
+        });        
+        $A.enqueueAction(action);        
+    },
+
+    sortTable: function(component, event, helper) {
+        var fieldName = event.currentTarget.name;
+        var currentOrder = component.get("v.sortAsc"),
+            currentList = component.get("v.allCustomers");
+        currentOrder = !currentOrder;
+        currentList.sort(function(a,b) {
+            var t1 = a[fieldName] == b[fieldName], t2 = a[fieldName] < b[fieldName];
+            return t1? 0: (currentOrder?-1:1)*(t2?1:-1);
+        });
+        component.set("v.sortAsc", currentOrder);
+        component.set("v.allCustomers", currentList);
+        component.set("v.sortField", fieldName);
+    },
+
     openCustomerWindow : function(component, event, helper) {          
         var allCustomers = component.find("allCustomers");   
         $A.util.addClass(allCustomers, 'noDisplayBar');
