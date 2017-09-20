f31c8d6347b42c2bb9f23fa78b37870131e48342
pyao-bwc <pyao@bluewave-capital.com>
2017-09-20 07:03:33 -0400

    Remove unused display and displayOut functions

--- src/aura/LoanUnderwritingApplicantDebt/LoanUnderwritingApplicantDebtHelper.js
+++ src/aura/LoanUnderwritingApplicantDebt/LoanUnderwritingApplicantDebtHelper.js
@@ -25,11 +25,4 @@
             $A.enqueueAction(action);
         });
     },
-    
-    toggleHelper : function(component, event, auraId) {
-        var selectedItem = event.srcElement;
-        var targetId = selectedItem.name + '_help';
-        var targetElem = document.getElementById(targetId);
-        $A.util.toggleClass(targetElem, "toggle");
-    }
 })
