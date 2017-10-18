f998e7ec577199be733586cc2f7d19a364226671
jpentaleri <jpentaleri@bluewave-capital.com>
2017-10-18 16:30:57 -0400

    Portal Edits
    I didn't change the look, just cleaned up the controllers helpers and
    apex
    
    1. Only have bills that are marked as Published show up in the portal
    (that field is set by edits in sub-branch)
    2. Link to file is shown in the bill table
    3. Remove the autopay sign up form, which isn't live but shouldn't ever
    be
    4. Balance on the dashboard should match the balance on the my account
    page, so merged the MyAccountController and
    MyAccountSolarDashboardController

--- src/aura/MakeAPayment/MakeAPaymentHelper.js
+++ src/aura/MakeAPayment/MakeAPaymentHelper.js
@@ -1,5 +1,63 @@
 ({
-	helperMethod : function() {
-		
+	refreshTableData : function(component, accountId) {
+        console.log(accountId);
+        var actionGetSystemBills = component.get("c.getSystemBills");  
+        var actionGetAccountBills = component.get("c.getAccountBills");
+        var actionGetTransactions = component.get("c.getTransactions");
+        
+        actionGetSystemBills.setParams({
+			"propertyAccountId" : accountId
+        });
+
+       actionGetAccountBills.setParams({
+			"propertyAccountId" : accountId
+        });
+
+        actionGetTransactions.setParams({
+			"propertyAccountId" : accountId
+        });
+
+        actionGetSystemBills.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.SystemBills", resp.getReturnValue());
+            }
+            else {
+                $A.log("Errors", resp.getError());
+            }
+        }); 
+       
+        actionGetTransactions.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.PaymentLogs", resp.getReturnValue());
+                component.set("v.paymentLogs", resp.getReturnValue());
+            }
+            else {
+                $A.log("Errors", resp.getError());
+            }
+        }); 
+        
+        actionGetAccountBills.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.AccountBills", resp.getReturnValue());
+                accountBillList = resp.getReturnValue(); 
+       			var abStep;
+       			var totalOutstandingBalance = 0;
+        		if (accountBillList === undefined || accountBillList.length === 0) {
+        			component.set("v.myBill", 0);
+        		} else {
+	        		for (abStep = 0; abStep < accountBillList.length; abStep++) {
+	       				totalOutstandingBalance = totalOutstandingBalance + accountBillList[abStep].Balance_Net_Late_Payments__c;
+	       			}
+	       			var roundedBalance = totalOutstandingBalance.toFixed(2);
+	       			component.set("v.myBill", roundedBalance);
+        		}
+            }
+            else {
+                $A.log("Errors", resp.getError());
+            }
+        });        
+        $A.enqueueAction(actionGetAccountBills);
+        $A.enqueueAction(actionGetTransactions); 
+        $A.enqueueAction(actionGetSystemBills);
 	}
 })
--- src/aura/MyAccountSolarDashboard/MyAccountSolarDashboardController.js
+++ src/aura/MyAccountSolarDashboard/MyAccountSolarDashboardController.js
@@ -1,104 +1,24 @@
-({
-    dashboardMenuOutput : function(component, event) {            
+({  dashboardMenuOutput : function(component, event, helper) {    
         var label = event.getParam("propertyAccountId");
-        //var source = event.getSource();
-        //var label = source.get("v.label"); 
-       
-        var actionMyAccountBill = component.get("c.getDashboardBill"); //shows the total due for the property account selected
-		var actionMySavings = component.get("c.getDashboardSavings"); //shows the total savings for the property account selected
-		var actionMyProduction = component.get("c.getDashboardProduction"); //shows the total savings for the property account selected        
-                        
-        actionMyAccountBill.setParams({propertyAccountId : label});
- 		actionMySavings.setParams({propertyAccountId : label});
- 		actionMyProduction.setParams({propertyAccountId : label});         
-                                               
-        actionMyAccountBill.setCallback(this,function(resp){ //shows the total due for the property account selected
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.myBill", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });
-        actionMySavings.setCallback(this,function(resp){ //shows the total due for the property account selected
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.mySavings", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });
-        actionMyProduction.setCallback(this,function(resp){ //shows the total due for the property account selected
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.myProduction", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });         
-        $A.enqueueAction(actionMyAccountBill);
-        $A.enqueueAction(actionMySavings); 
-        $A.enqueueAction(actionMyProduction);         
-    }
-,    
-         
-	doInit : function(component, event, helper) {
-		//var action = component.get("c.getDashboardInfo");
-		var actionBill = component.get("c.getDashboardBill"); 
-		var actionSavings = component.get("c.getDashboardSavings");
-        var actionProduction = component.get("c.getDashboardProduction");
-        var actionGetMenuLabels = component.get("c.getMenuLabelList");        
-        
-        actionBill.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.myBill", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });
-        actionSavings.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.mySavings", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });
-        actionProduction.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.myProduction", resp.getReturnValue());
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-        });           
-        
-        actionGetMenuLabels.setCallback(this,function(resp){
+        if (label === null || label === undefined ) {
+            helper.refreshAccountMetrics(component, 'All');                                         
+        } else {
+            helper.refreshAccountMetrics(component, label); 
+        }
+    },
+
+    doInit : function(component, event, helper) {
+        var actionGetPropertyAccounts = component.get("c.getMenuLabelList");         
+        actionGetPropertyAccounts.setCallback(this,function(resp){
             if(resp.getState() == 'SUCCESS') {
                 component.set("v.menuLabels", resp.getReturnValue());
             }
             else {
                 $A.log("Errors", resp.getError());
             }
-        });          
-               	$A.enqueueAction(actionGetMenuLabels); 
-    	/*action.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-        		component.set("v.myBill", resp.getReturnValue().myBill);
-                component.set("v.mySavings", resp.getReturnValue().mySavings);
-                component.set("v.myConsumption", resp.getReturnValue().myConsumption);
-                component.set("v.myProduction", resp.getReturnValue().myProduction);
-            }
-            else {
-                $A.log("Errors", resp.getError());
-            }
-    	});*/
-         
-	    //$A.enqueueAction(action);
- 		$A.enqueueAction(actionBill);
- 		$A.enqueueAction(actionSavings);
- 		$A.enqueueAction(actionProduction);         
-        
-	}
+        });    
+        $A.enqueueAction(actionGetPropertyAccounts);
+        console.log("HELLO!");
+        helper.refreshAccountMetrics(component, 'All');
+    }
 })
