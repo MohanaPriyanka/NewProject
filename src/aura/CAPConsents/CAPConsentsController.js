9fae68c37b971ee589ea7478194dd7204bca047e
cmswain <cswain@bluewave-capital.com>
2017-12-29 12:00:30 -0500

    SREC Page confirm/selection

--- src/aura/CAPConsents/CAPConsentsController.js
+++ src/aura/CAPConsents/CAPConsentsController.js
@@ -6,6 +6,17 @@
         } else {
             helper.handleNavEvent(component, event, helper, 'Disbursal');
         }
+        var action = component.get("c.getProducts");
+        action.setParams({state: component.get("v.lead.LASERCA__Home_State__c"),
+                          productType: 'SREC'});
+        action.setCallback(this,function(resp){
+            if (resp.getState() == 'SUCCESS') {
+                component.set("v.availableSRECProducts", resp.getReturnValue());
+            } else {
+                helper.logError("SLPSendApplicationEmailController", "availableSRECProducts", resp.getError());
+            }
+        });
+        $A.enqueueAction(action);
     },
 
     saveSRECAskDisbursal : function(component, event, helper) {
@@ -23,5 +34,19 @@
         leadPromise.then($A.getCallback(function resolve(retVal) {
             helper.finishStage(component, event, helper);
         }));
-    },
+    }, 
+
+    // getAvailableSRECProducts : function(component, event, helper) { 
+    //     var action = component.get("c.getProducts");
+    //     action.setParams({state: component.get("v.lead.LASERCA__Home_State__c"),
+    //                       productType: 'SREC'});
+    //     action.setCallback(this,function(resp){
+    //         if (resp.getState() == 'SUCCESS') {
+    //             component.set("v.availableSRECProducts", resp.getReturnValue());
+    //         } else {
+    //             helper.logError("SLPSendApplicationEmailController", "availableSRECProducts", resp.getError());
+    //         }
+    //     });
+    //     $A.enqueueAction(action);
+    // },    
 })
