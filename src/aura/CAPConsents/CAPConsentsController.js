e3fcbc87fd25a8379744e750c0c0a05e1a180990
pyao-bwc <pyao@bluewave-capital.com>
2018-01-08 11:50:49 -0500

    Ask for TC on MSLP application; Handle siteURL for portal and internal users

--- src/aura/CAPConsents/CAPConsentsController.js
+++ src/aura/CAPConsents/CAPConsentsController.js
@@ -1,12 +1,40 @@
 ({
     handleNavEvent : function(component, event, helper) {
         const lead = event.getParam('lead');
-        if (lead && lead.LASERCA__Home_State__c === 'MA') {
-            helper.handleNavEvent(component, event, helper, 'SREC');
-            helper.getSRECProducts(component, event, helper, lead);
-        } else {
-            helper.handleNavEvent(component, event, helper, 'Disbursal');
+        if (event.getParam("eventType") === "INITIATED" &&
+            event.getParam("stageName") === component.get("v.STAGENAME")) {
+            if (lead && lead.LASERCA__Home_State__c === 'MA') {
+                if (lead.Product_Program__c === 'MSLP') {
+                    helper.handleNavEvent(component, event, helper, 'MSLPTechnicalConfirm');
+                    helper.parseAttachments(component, helper);
+                } else {
+                    helper.handleNavEvent(component, event, helper, 'SREC');
+                }
+                helper.getSRECProducts(component, event, helper, lead);
+            } else {
+                helper.handleNavEvent(component, event, helper, 'Disbursal');
+            }
         }
+    },
+
+    handleTC : function(component, event, helper) {
+        helper.handleAttachment(component, event, helper, helper.TC_DOC);
+    },
+
+    saveTCAskSREC : function(component, event, helper) {
+        if (helper.checkProjectIDErrors(component)) {
+            helper.logError("CAPConsentsController", "saveTCAskSREC", helper.checkProjectIDErrors(component));
+            return;
+        }
+
+        const lead = component.get('v.lead');
+        var leadToSave = {
+            sobjectType: 'Lead',
+            Id: lead.Id,
+            Project_Identification_Number__c: lead.Project_Identification_Number__c
+        };
+        helper.saveSObject(component, lead.Id, 'Lead', null, null, leadToSave);
+        component.set('v.page', 'SREC');
     },
 
     saveSRECAskDisbursal : function(component, event, helper) {
