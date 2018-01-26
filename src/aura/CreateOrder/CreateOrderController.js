234139a11443fcbf029e51345fa4448039735b58
jpentaleri <jpentaleri@bluewave-capital.com>
2018-01-26 13:55:17 -0500

    Need to confirm Email Address First

--- src/aura/CreateOrder/CreateOrderController.js
+++ src/aura/CreateOrder/CreateOrderController.js
@@ -1,10 +1,17 @@
 ({
-    doInit : function(component, event, helper) {
-        const opportunity = component.get("v.OppId");
-        const PaymentRequestPromise = helper.insertRecord(component, opportunity);
-        PaymentRequestPromise.then($A.getCallback(function resolve(retVal) {
-           var docInterval = window.setInterval($A.getCallback(function() {
-               helper.checkForLink(component,retVal.Id);}), 2000);
-       }));
+    checkLoanNum : function(component, event, helper) {
+    	const oppID = component.get("v.OppId");
+        const emailInput = component.get("v.emailInput");
+
+    	const IDPromise = helper.checkID(component, oppID, emailInput);
+        IDPromise.then($A.getCallback(function resolve(retVal) {
+    		console.log(retVal);
+	        const opportunity = component.get("v.OppId");
+	        const PaymentRequestPromise = helper.insertRecord(component, opportunity);    
+	        PaymentRequestPromise.then($A.getCallback(function resolve(retVal) {
+	           var docInterval = window.setInterval($A.getCallback(function() {
+	               helper.checkForLink(component,retVal.Id);}), 2000);
+	       }));    	
+	    }));
     },
 })
--- src/aura/CreateOrder/CreateOrderHelper.js
+++ src/aura/CreateOrder/CreateOrderHelper.js
@@ -2,22 +2,19 @@
     insertRecord : function(component, oppId){
         return new Promise(function(resolve, reject) {
             const actionInsert = component.get("c.insertRequest");
-            
+
             actionInsert.setParams({
                 "oppId": oppId
             }); 
-            
+
             actionInsert.setCallback(this,function(response) {
                 if(response.getState() == "SUCCESS") { 
                     const retVal = response.getReturnValue();
                     resolve(retVal);
+                    console.log('SUCCESS');
                 } else {
-                    const appEvent = $A.get("e.c:ApexCallbackError");
-                    appEvent.setParams({"className" : "BlueWaveParentHelper",
-                                            "methodName" : "insertSObject",
-                                            "errors" : resp.getError(),
-                                            "developerInfo" : sObject});
-                    appEvent.fire();
+                    console.log('ERROR');
+                    console.log(response.getReturnValue());
                 }
             });                                 
             $A.enqueueAction(actionInsert);
@@ -34,11 +31,33 @@
         actionGetUrl.setCallback(this,function(response) {
             if(response.getState() == "SUCCESS" && response.getReturnValue().ChargentOrders__Pay_Link__c != null) { 
                 component.set("v.PaymentLink", response.getReturnValue().ChargentOrders__Pay_Link__c);
-                component.set("v.showButton", true);
+                component.set("v.showEmail", false);
             } else {
                 console.log('failfail');
             }
         });                                 
         $A.enqueueAction(actionGetUrl);
+    },
+
+    checkID : function(component, oppId, emailInput){
+        return new Promise(function(resolve, reject) {
+            const actionCheckID = component.get("c.checkEmail");
+            
+            actionCheckID.setParams({
+                "oppId": oppId,
+                "emailInput": emailInput
+            }); 
+            
+            actionCheckID.setCallback(this,function(response) {
+                if(response.getState() == "SUCCESS" && response.getReturnValue() == true) { 
+                    const retVal = response.getReturnValue();
+                    resolve(retVal);
+                } else {
+                    $A.util.addClass(component.find('Confirm'), 'shake slds-has-error');
+                    component.set("v.showError", true); 
+                }
+            });                                 
+            $A.enqueueAction(actionCheckID);
+        });
     },
 })
