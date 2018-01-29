167f1fdf53f1b2cb14b68d4bdaa027805869ca52
jpentaleri <jpentaleri@bluewave-capital.com>
2018-01-29 15:02:02 -0500

    Edits from Peter's comments

--- src/aura/CreateOrder/CreateOrderController.js
+++ src/aura/CreateOrder/CreateOrderController.js
@@ -3,6 +3,7 @@
     	const oppID = component.get("v.OppId");
         const emailInput = component.get("v.emailInput");
 
+    	var IDPromise = helper.insertRecord(component, oppID, emailInput, helper);
         IDPromise.then($A.getCallback(function resolve(retVal) {
 			var docInterval = window.setInterval($A.getCallback(function() {
 	            helper.checkForLink(component,retVal, helper);
