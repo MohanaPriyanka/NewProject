3f53760367e12eae55b8aa7b1d3d4eb89ef1e00e
jpentaleri <jpentaleri@bluewave-capital.com>
2018-01-29 15:01:49 -0500

    Edits from Peter's comments

--- src/aura/CreateOrder/CreateOrderController.js
+++ src/aura/CreateOrder/CreateOrderController.js
@@ -3,15 +3,11 @@
     	const oppID = component.get("v.OppId");
         const emailInput = component.get("v.emailInput");
 
-    	const IDPromise = helper.checkID(component, oppID, emailInput);
         IDPromise.then($A.getCallback(function resolve(retVal) {
-    		console.log(retVal);
-	        const opportunity = component.get("v.OppId");
-	        const PaymentRequestPromise = helper.insertRecord(component, opportunity);    
-	        PaymentRequestPromise.then($A.getCallback(function resolve(retVal) {
-	           var docInterval = window.setInterval($A.getCallback(function() {
-	               helper.checkForLink(component,retVal.Id);}), 2000);
-	       }));    	
+			var docInterval = window.setInterval($A.getCallback(function() {
+	            helper.checkForLink(component,retVal, helper);
+	        }), 2000);
+	       	window.setTimeout(function() {clearInterval(docInterval)},8000)
 	    }));
     },
 })
--- src/aura/CreateOrder/CreateOrderHelper.js
+++ src/aura/CreateOrder/CreateOrderHelper.js
@@ -1,27 +1,32 @@
 ({  
-    insertRecord : function(component, oppId){
-        return new Promise(function(resolve, reject) {
+    insertRecord : function(component, oppId, emailInput, helper){
+        $A.util.removeClass(component.find('Confirm'), 'shake slds-has-error');
+        component.set("v.showError", false); 
+       
+       return new Promise(function(resolve, reject) {
             const actionInsert = component.get("c.insertRequest");
 
             actionInsert.setParams({
-                "oppId": oppId
+                "oppId": oppId,
+                "emailInput": emailInput
             }); 
 
             actionInsert.setCallback(this,function(response) {
-                if(response.getState() == "SUCCESS") { 
+                if(response.getState() === "SUCCESS" && response.getReturnValue() === 'emailFailure') { 
+                    $A.util.addClass(component.find('Confirm'), 'shake slds-has-error');
+                    component.set("v.showError", true); 
+                } else if (response.getState() === "SUCCESS") {
                     const retVal = response.getReturnValue();
                     resolve(retVal);
-                    console.log('SUCCESS');
                 } else {
-                    console.log('ERROR');
-                    console.log(response.getReturnValue());
+                    helper.logError('CreateOrderandPaymentRequest','insertRequest', response.getReturnValue(), '');
                 }
             });                                 
             $A.enqueueAction(actionInsert);
         });
     },
     
-    checkForLink : function(component, payRequestId){
+    checkForLink : function(component, payRequestId, helper){
         const actionGetUrl = component.get("c.checkForLink");
         
         actionGetUrl.setParams({
@@ -29,38 +34,13 @@
         }); 
         
         actionGetUrl.setCallback(this,function(response) {
-            if(response.getState() == "SUCCESS" && response.getReturnValue().ChargentOrders__Pay_Link__c != null) { 
+            if(response.getState() === "SUCCESS" && response.getReturnValue().ChargentOrders__Pay_Link__c != null) { 
                 component.set("v.PaymentLink", response.getReturnValue().ChargentOrders__Pay_Link__c);
                 component.set("v.showEmail", false);
             } else {
-                console.log('failfail');
+                helper.logError('CreateOrderandPaymentRequest','checkForLink', response.getReturnValue(), '');
             }
         });                                 
         $A.enqueueAction(actionGetUrl);
-    },
-
-    checkID : function(component, oppId, emailInput){
-        $A.util.removeClass(component.find('Confirm'), 'shake slds-has-error');
-        component.set("v.showError", false); 
-
-        return new Promise(function(resolve, reject) {
-            const actionCheckID = component.get("c.checkEmail");
-            
-            actionCheckID.setParams({
-                "oppId": oppId,
-                "emailInput": emailInput
-            }); 
-            
-            actionCheckID.setCallback(this,function(response) {
-                if(response.getState() == "SUCCESS" && response.getReturnValue() == true) { 
-                    const retVal = response.getReturnValue();
-                    resolve(retVal);
-                } else {
-                    $A.util.addClass(component.find('Confirm'), 'shake slds-has-error');
-                    component.set("v.showError", true); 
-                }
-            });                                 
-            $A.enqueueAction(actionCheckID);
-        });
     },
 })
