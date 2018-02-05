f6ddcc703dc2cb466c77f4198cd2c93f04055060
jpentaleri <jpentaleri@bluewave-capital.com>
2018-02-05 11:24:32 -0500

    Community Solar Tab & Pop Up Window

--- src/aura/SLPNavigationBar/SLPNavigationBarController.js
+++ src/aura/SLPNavigationBar/SLPNavigationBarController.js
@@ -4,16 +4,60 @@
         var actionLicenseType = component.get("c.getLicenseType");        
         actionLicenseType.setCallback(this,function(resp){
             if(resp.getState() == 'SUCCESS') {
-                if (resp.getReturnValue() && resp.getReturnValue() == 'Executive') {
+                const partnerProfile = resp.getReturnValue();
+                const listParams = partnerProfile.split("/",3);
+                const licenseType = listParams[0];
+                const referralCode = listParams[1];
+                const disableCS = listParams[2];
+                if (licenseType == 'Executive'){
                     component.set("v.licenseType", true);
                     $A.util.removeClass(component.find("disbursalsMenuItem"), 'noDisplay');
+                }
+                if (disableCS === 'false'){
+                    component.set("v.referralCode", referralCode);
+                } else {
+                    component.set("v.showCSTab", false);
                 }
             }    
             else {
                 $A.log("Errors", resp.getError());
             }
         });    
-        $A.enqueueAction(actionLicenseType);              
+        $A.enqueueAction(actionLicenseType);                      
+    },
+
+    openCSWindow : function(component, event, helper) {
+        component.set("v.CSWindow", true);  
+        component.set("v.emailSuccess", false);    
+    },
+
+    closeCSWindow : function(component, event, helper) {
+        component.set("v.CSWindow", false);   
+    },
+
+    sendCSApplication : function(component, event, helper) {
+        var actionSendEmail = component.get("c.sendAnEmail");        
+        const toEmail = component.get("v.emailInput");
+        const salesCode = component.get("v.referralCode");
+
+        actionSendEmail.setParams({
+            "emailTemplateName": 'SLP_Community_Solar_Application',
+            "orgWideEmail": 'customercare@bluewavesolar.com',
+            "subjectLine": 'Start Your Community Solar Application Here',
+            "addressList": toEmail,
+            "templateField" : '{!ReferralCode}',
+            "replaceText" : salesCode,
+        }); 
+
+        actionSendEmail.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS' && resp.getReturnValue() == 'EmailSent') {
+                component.set("v.emailSuccess", true);   
+            } else {
+                alert('Please enter a valid email address');
+            }
+        });    
+
+        $A.enqueueAction(actionSendEmail);   
     },
 
     hideDisplay : function(component, event, helper) {
