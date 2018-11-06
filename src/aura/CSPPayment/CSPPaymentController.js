d5d5afd42021b372ab70836e568bdf4ec39c9b8e
Max Stackhouse <maxstackhouse@gmail.com>
2018-11-06 15:53:02 -0700

    Clear fields on change (#637)
    ### Work Items Covered
    <!-- Please provide links to the work items covered in PR -->
    #### Bug Fixes
    [CC and ACH saving](https://na54.salesforce.com/a3U0a000000t90bEAA)
    
    Moved into own branch so we can release
    
    ### Deployment Steps
    <!-- If there are configuration changes necessary -->
    
    ### How has this been tested?
    - [x] Deployment has been Tested
    - [ ] Automated Tests <!-- Describe Testing Approach -->
    - [x] Manual Tests <!-- Provide any steps needed to test this PR manually -->
    
    ![image](https://user-images.githubusercontent.com/29938166/48085221-e8c01500-e1b6-11e8-9231-059e2aceb1c4.png)
    
    
    ### Other Information
    <!--
    Provide additional information relevant to reviewers ex.:
    - Justification for no tests
    - Alternate approaches considered
    - Changes that Break existing features
    -->
    
    <!--
    [Markdown Cheatsheet](https://enterprise.github.com/downloads/en/markdown-cheatsheet.pdf)
    -->

--- src/aura/CSPPayment/CSPPaymentController.js
+++ src/aura/CSPPayment/CSPPaymentController.js
@@ -61,4 +61,26 @@
         }
     },
 
+    clearFields : function(component, event, helper) {
+        let chOrder = component.get('v.chOrder');
+        component.set('v.chOrder', { 'sobjectType': 'ChargentOrders__ChargentOrder__c',
+            'ChargentOrders__Payment_Method__c' : '',
+            'ChargentOrders__Bank_Name__c' : '',
+            'ChargentOrders__Bank_Routing_Number__c' : '',
+            'ChargentOrders__Bank_Account_Type__c' : '',
+            'ChargentOrders__Bank_Account_Number__c' : '',
+            'ChargentOrders__Bank_Account_Name__c' : '',
+            'ChargentOrders__Billing_Email__c' : 'compliance@bluewavesolar.com',
+            'ChargentOrders__Card_Type__c' : '',
+            'ChargentOrders__Card_Number__c' : '',
+            'ChargentOrders__Card_Expiration_Month__c' : '',
+            'ChargentOrders__Card_Expiration_Year__c' : '',
+            'ChargentOrders__Billing_Address__c' : '',
+            'ChargentOrders__Billing_City__c' : '',
+            'ChargentOrders__Billing_State__c' : '',
+            'ChargentOrders__Billing_Zip_Postal__c' : '',
+            'ChargentOrders__Billing_First_Name__c' : '',
+            'ChargentOrders__Billing_Last_Name__c' : '',
+            'Autopay_Only__c': chOrder.Autopay_Only__c});
+    }
 })
