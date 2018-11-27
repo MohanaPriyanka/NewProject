152d765ccd8aa679df29f45033c882c92cded098
Alex Barnes <abarnes@bluewavesolar.com>
2018-11-27 19:30:44 +0000

    Adjust DTI on Lead to 50% to allow for product selection and revert back after income docs are uploaded

--- src/aura/CAPIncomeDoc/CAPIncomeDocController.js
+++ src/aura/CAPIncomeDoc/CAPIncomeDocController.js
@@ -439,12 +439,18 @@
     finishStage : function(component, event, helper) {
         const lead = component.get('v.lead');
         let leadToSave;
+        let leadPCR;
         if (lead.Status === 'Awaiting Info Requested from Customer') {
             leadToSave = {
                 sobjectType: 'Lead',
                 Status: 'Under BlueWave Review',
                 Id: lead.Id,
                 Unfinished_Lead__c: false
+            };
+            leadPCR = {
+              sobjectType: 'LASERCA__Personal_Credit_Report__c',
+              Id: lead.Personal_Credit_Report__c,
+              Adjusted_DTI__c: null
             };
         } else {
             leadToSave = {
@@ -454,6 +460,17 @@
             };
         }
         helper.saveSObject(component, lead.Id, 'Lead', null, null, leadToSave)
+        .then($A.getCallback(function resolve() {
+            if (leadPCR !== null) {
+                helper.saveSObject(component,
+                    lead.Personal_Credit_Report__c,
+                    'LASERCA__Personal_Credit_Report__c',
+                    null,
+                    null,
+                    leadPCR
+                );
+            }
+        }))
         .then($A.getCallback(function resolve() {
             return helper.insertPartnerTaskFunction(component, event, helper);
         }))
