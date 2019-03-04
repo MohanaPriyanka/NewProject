6aad979779ec63f2541ac9b2f1b875961ae9cba9
Sarah Renfro <srenfro@bluewavesolar.com>
2019-03-04 16:52:03 +0000

    US-0000356: CSAP 2.1 Final Copy

--- src/aura/CSAPStart/CSAPStartController.js
+++ src/aura/CSAPStart/CSAPStartController.js
@@ -1,24 +1,40 @@
 ({
     doInit: function(component, event, helper) {
         component.set("v.STAGENAME", "NAV_Getting_Started");
-        component.set("v.page", "ApplicationPrep");
+        component.set("v.page", "ZipcodeChecker");
         var leadId = component.get("v.leadId");
-        if(leadId){
+        if (leadId != null && leadId != '' && leadId != undefined) {
             component.set("v.page","Login");
         }
     },
 
     handleNavEvent : function(component, event, helper) {
+        const options = event.getParam("options");
+        if (options && options.pageName) {
+            helper.handleNavEvent(component, event, helper, options.pageName);
+        }else{
+            helper.handleNavEvent(component, event, helper, "ZipcodeChecker");
+        }
+    },
+
+    capacityChecked : function(component, event, helper) {
+        var hasCapacity = component.get('v.hasCapacity');
+        if (hasCapacity) {
+            var lead = component.get("v.lead");
+            component.set("v.page", "AboutYourself");
+            helper.raiseNavEvent("INITIATED", {"stageName": "NAV_Personal_Information", "lead": lead, "page": "AboutYourself"});
+        } else {
+            component.set("v.page", "NoProjects");
+        }
     },
 
     beginApplication : function(component, event, helper) {
-        var lead = component.get("v.lead");
-        component.set("v.page", "AboutYourself");
-        helper.raiseNavEvent("INITIATED", {"stageName": "NAV_Personal_Information", "lead": lead, "page": "AboutYourself"});
+        component.set("v.page", "ZipcodeChecker");
     },
 
     checkForEnter : function(component, event, helper) {
-        if (event.getParams().keyCode == 13) {
+        if (event.which === 13) {
+            event.preventDefault();
             helper.login(component, event, helper);
         }
     },
