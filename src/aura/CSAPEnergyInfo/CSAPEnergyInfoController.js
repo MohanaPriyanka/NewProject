ced09924385bdedd30be1ebbb186a678011169ba
Sarah Renfro <srenfro@bluewavesolar.com>
2019-03-18 18:13:35 +0000

    US-0000395: CSAP 2.2: Remove "Add another utility"

--- src/aura/CSAPEnergyInfo/CSAPEnergyInfoController.js
+++ src/aura/CSAPEnergyInfo/CSAPEnergyInfoController.js
@@ -42,39 +42,7 @@
     goToUtilityAccountInformation : function(component, event, helper) {
         component.set("v.page", "UtilityAccountInformation");
     },
-    goToAddMore : function(component, event, helper) {
-        //Upsert UAL record
-        if(helper.validatePageFields(component)){
-            var ual = component.get("v.ual");
-            var lead = component.get("v.lead");
 
-
-            if(ual.Lead__c == null){
-                ual.Lead__c = lead.Id;
-            }
-            var utility = component.get('v.utility');
-            
-            ual.Utility__c = utility.Id;
-            var saveUAL = component.get('c.saveUtilityAccountLog');
-            saveUAL.setParams({'ual' : ual});
-            saveUAL.setCallback(this, function(resp) {
-                if (resp.getState() === 'SUCCESS') {
-                    component.set("v.page", "AddMoreUAL");
-                    component.set("v.loading", false);
-                }else{
-                    helper.logError("CSAPEnergyInfoController", "goToAddMore", resp.getError(), lead);
-                }
-            });
-
-            if(ual.Rate_Class__c === 'Rate Class Not in List') {
-                alert('Only rate classes in the list are eligible for this product. Please close this window and mark the lead as unqualified.');
-            } else {
-                component.set("v.loading", true);
-                component.set("v.loadingText", "Saving utility account information...");
-                $A.enqueueAction(saveUAL);
-            }
-        }
-    },
     cancelAddUAL : function(component, event, helper) {
         if(confirm("Are you sure you want to cancel adding another Utility Account Log?")){
             helper.finishStage(component, event, helper);
@@ -94,20 +62,56 @@
     },
 
     finishStage : function(component, event, helper) {
-        var lead = component.get("v.lead");
-        var action = component.get('c.sendEmailForPaymentInfo');
-        action.setParams({
-            "lead": lead
-        });
-        action.setCallback(this, function(resp) {
-            if (resp.getState() === "SUCCESS") {
-                component.set('v.page', 'CompletedUtilityInfo');
-            } else {
-                helper.logError("CSAPEnergyInfoController", "finishStage",
-                    "There was an issue saving this information. We have recorded this error and will review it.",
-                    resp.getError());
+        if(helper.validatePageFields(component)){
+            var lead = component.get("v.lead");
+            var ual = component.get("v.ual");
+
+
+            if(ual.Lead__c == null){
+                ual.Lead__c = lead.Id;
             }
-        })
-        $A.enqueueAction(action);
+
+            var utility = component.get('v.utility');
+            ual.Utility__c = utility.Id;
+
+            var saveUAL = component.get('c.saveUtilityAccountLog');
+            saveUAL.setParams({'ual' : ual});
+            saveUAL.setCallback(this, function(resp) {
+                if (resp.getState() === 'SUCCESS') {
+                    //component.set("v.page", "AddMoreUAL");
+                    //component.set("v.loading", false);
+                }else{
+                    helper.logError("CSAPEnergyInfoController", "goToAddMore", resp.getError(), lead);
+                }
+            });
+
+            if(ual.Rate_Class__c === 'Rate Class Not in List') {
+                alert('Only rate classes in the list are eligible for this product. Please close this window and mark the lead as unqualified.');
+            } else {
+                component.set("v.loading", true);
+                component.set("v.loadingText", "Saving utility account information...");
+                $A.enqueueAction(saveUAL);
+            }
+
+            var action = component.get('c.sendEmailForPaymentInfo');
+            action.setParams({
+                "lead": lead
+            });
+            action.setCallback(this, function(resp) {
+                if (resp.getState() === "SUCCESS") {
+                    component.set("v.loading", false);
+                    component.set('v.page', 'CompletedUtilityInfo');
+                } else {
+                    helper.logError("CSAPEnergyInfoController", "finishStage",
+                        "There was an issue saving this information. We have recorded this error and will review it.",
+                        resp.getError());
+                }
+            })
+            $A.enqueueAction(action);
+        }
+
+
+
+
     },
 })
