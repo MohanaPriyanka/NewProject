28413facb648437077b5ec7ad054c387fa7b8f68
Sarah Renfro <srenfro@bluewavesolar.com>
2019-03-22 16:47:09 +0000

    US-0000414: CSAP 2.2 Navigation Bar

--- src/aura/CSAPNavigationBar/CSAPNavigationBarController.js
+++ src/aura/CSAPNavigationBar/CSAPNavigationBarController.js
@@ -5,13 +5,47 @@
         if (leadId != null && leadId != '' && leadId != undefined) {
             component.set("v.showNavBar", false);
         }
+
+        var partnerId = component.get("v.partnerId");
+
+        if (partnerId != null && partnerId != '' && partnerId != undefined) {
+            var action = component.get("c.checkApplicationPartner");
+            action.setParams({"partnerId": component.get("v.partnerId")});
+            action.setCallback(this, function(resp) {
+                console.log('Result: ' + resp.getState());
+                if(resp.getState() === 'SUCCESS') {
+                    component.set("v.partnerApp", resp.getReturnValue());
+                    console.log(' Nav Bar with ID: ' + component.get("v.partnerApp"));
+                } else {
+                    this.logError('CSAPApplication', 'checkApplicationPartner', resp.getError(), component.get('v.partnerId'));
+                }
+            });
+            $A.enqueueAction(action);
+
+        }
+
+        //if partner Id is populated, set partnerApp
+
+        //if partnerId is NOT populated, need to check Source from Lead to set partnerApp
+
     },
 
     handleNavEvent : function(component, event, helper) {
         component.set("v.showNavBar", true);
+        var lead;
         if (event.getParam("lead")) {
             component.set("v.lead", event.getParam("lead"));
+            lead = event.getParam("lead");
         }
+        console.log('What is partner APp here? ' + component.get("v.partnerApp"));
+        if (component.get("v.partnerApp") == undefined) {
+            if ( lead.Application_Source_Phase_1__c == 'CSAP 2.1 with Partner'){
+                component.set("v.partnerApp", true);
+            } else {
+                component.set("v.partnerApp", false);
+            }
+        }
+
 
         if (event.getParam("eventType") === "INITIATED") {
             //Get current stage to set current index
