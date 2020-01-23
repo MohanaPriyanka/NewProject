9b19916253a3cbfa3af5a7281f135d3258815e84
Jordan Pentaleri <jpentaleri@bluewavesolar.com>
2020-01-23 19:16:49 +0000

    US-0001119: W-011135: Bill off Transfer Sheet: Create LWC, Upload and Read File

---
+++ src/lwc/bluewaveFileUpload/bluewaveFileUpload.js
@@ -0,0 +1,26 @@
+import { LightningElement, track, api, wire} from 'lwc';
+import updateFileCategory from '@salesforce/apex/ContentService.setCategoryOnContentVersion';
+
+export default class bwFileUpload extends LightningElement {
+    @api recordId;
+    @api categoryType;
+    @api acceptedFileTypes;
+
+    get acceptedFormats() {
+        return this.acceptedFileTypes;
+    }
+
+    handleUploadFinished(event) {
+        const uploadedFiles = event.detail.files;
+        updateFileCategory({
+            contentDocumentId: uploadedFiles[0].documentId,
+            category: this.categoryType
+        })
+        .then(() => {
+            alert("Uploaded!");
+        })
+        .catch((error) => {
+            alert('Could not update Category on File: error' + error.errorCode + ', ' + error.body.message);
+        });
+    }
+}
