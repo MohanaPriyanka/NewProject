d6ecc5e42afaa558de09b34cadf7a8a79738fd98
Jordan Pentaleri <jpentaleri@bluewavesolar.com>
2020-01-27 16:17:58 +0000

    handle multiple files

--- src/lwc/bluewaveFileUpload/bluewaveFileUpload.js
+++ src/lwc/bluewaveFileUpload/bluewaveFileUpload.js
@@ -1,5 +1,6 @@
 import { LightningElement, track, api, wire} from 'lwc';
 import updateFileCategory from '@salesforce/apex/ContentService.setCategoryOnContentVersion';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
 export default class bwFileUpload extends LightningElement {
     @api recordId;
@@ -12,12 +13,21 @@ export default class bwFileUpload extends LightningElement {
 
     handleUploadFinished(event) {
         const uploadedFiles = event.detail.files;
+        let idList = [];
+        var i;
+        for (i = 0; i < uploadedFiles.length; i++) {
+            idList.push(uploadedFiles[i].documentId);
+        }
         updateFileCategory({
-            contentDocumentId: uploadedFiles[0].documentId,
-            category: this.categoryType
+            documentIdList : idList,
+            category : this.categoryType
         })
         .catch((error) => {
-            alert('Could not update Category on File: error' + error.errorCode + ', ' + error.body.message);
+            const event = new ShowToastEvent({
+                title: 'Could not update Category on File: error',
+                message: error.errorCode + ', ' + error.body.message,
+            });
+            this.dispatchEvent(event);
         });
     }
 }
