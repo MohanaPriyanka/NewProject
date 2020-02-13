a350109ac6770c051aedc2b64c45f54341ba96e3
Jordan Pentaleri <jpentaleri@bluewavesolar.com>
2020-02-13 21:14:07 +0000

    US-0001209: W-016388: Bill off Transfer Sheet: LWC Match Screen

---
+++ src/lwc/transferSheetResolution/transferSheetResolution.js
@@ -0,0 +1,23 @@
+import { LightningElement, api} from 'lwc';
+
+export default class bwPicklist extends LightningElement {
+    @api recordNumber;
+    @api picklistOptions;
+
+    get picklistOptions() {
+        return [
+            { label: 'Use Utility', value: 'UseUtility' },
+            { label: 'Use Salesforce', value: 'UseSalesforce' },
+        ];
+    }
+
+    handlechange(event) {
+        if (event.detail.value == 'UseUtility'){
+            const utilEvent = new CustomEvent('useutility', {detail: this.recordNumber});
+            this.dispatchEvent(utilEvent);
+        } else if (event.detail.value == 'UseSalesforce'){
+            const sfEvent = new CustomEvent('usesalesforce', {detail: this.recordNumber});
+            this.dispatchEvent(sfEvent);
+        }
+    }
+}
