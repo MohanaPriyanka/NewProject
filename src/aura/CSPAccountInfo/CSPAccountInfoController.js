5d14131e21a84d1612cd5d240b788242c6cdba5a
Jordan Pentaleri <jpentaleri@bluewavesolar.com>
2020-02-14 16:46:33 +0000

    US-0001212: W-009261: CS Paper Bill Opt-Out

--- src/aura/CSPAccountInfo/CSPAccountInfoController.js
+++ src/aura/CSPAccountInfo/CSPAccountInfoController.js
@@ -4,6 +4,19 @@
         action.setCallback(this,function(resp){
             if(resp.getState() == 'SUCCESS') {
                 component.set("v.userContact", resp.getReturnValue());
+                var checkPaperless = component.get("c.isPaperless");
+                checkPaperless.setParams({
+                    "parentAccountId": resp.getReturnValue().AccountId
+                });
+                checkPaperless.setCallback(this,function(response){
+                    if(response.getState() == 'SUCCESS') {
+                        component.set("v.paperlessBill",response.getReturnValue());
+                    }
+                    else {
+                        $A.log("Errors", response.getError());
+                    }
+                });
+                $A.enqueueAction(checkPaperless);
             }
             else {
                 $A.log("Errors", resp.getError());
@@ -34,5 +47,26 @@
             }
         });
         $A.enqueueAction(updateAction);
+    },
+
+    goPaperless : function(component, event, helper) {
+        var setPropertiesToPaperless = component.get("c.setToPaperless");
+        let contact = component.get("v.userContact");
+        console.log(contact.AccountId);
+        console.log('goPaperless');
+
+        setPropertiesToPaperless.setParams({
+            "parentAccountId": contact.AccountId
+        });
+
+        setPropertiesToPaperless.setCallback(this,function(resp){
+            if(resp.getState() == 'SUCCESS') {
+                component.set("v.paperlessBill",true);
+            }
+            else {
+                $A.log("Errors", resp.getError());
+            }
+        });
+        $A.enqueueAction(setPropertiesToPaperless);
     },
 })
