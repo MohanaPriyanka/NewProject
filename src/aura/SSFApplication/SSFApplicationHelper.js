61f088109d2a5e8cbc1c2a57b0388e3415fb2d04
Jeffrey Parlin <jparlin@bluewavesolar.com>
2020-10-27 18:21:48 +0000

    US-0001885: W-020708: Contract Resigns with SSF

--- src/aura/SSFApplication/SSFApplicationHelper.js
+++ src/aura/SSFApplication/SSFApplicationHelper.js
@@ -1,35 +1,47 @@
-/**
- * Created by PeterYao on 3/9/2020.
- */
-
 ({
-    savePaymentMethodToAccount : function(propertyAccount) {
-        return new Promise($A.getCallback(function (resolve, reject) {
-            let calloutURI = '/apply/services/apexrest/v3/property-accounts';
-            const xmlHttpRequest = new XMLHttpRequest();
-            xmlHttpRequest.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    const response = JSON.parse(this.responseText);
-                    if (this.status === 200 || this.status === 201) {
-                        if (response.data) {
-                            resolve(response.data);
-                        } else {
-                            reject(this.responseText);
-                        }
-                    } else {
-                        reject(this.responseText);
-                    }
-                }
-            };
-            xmlHttpRequest.open('PATCH', calloutURI, true);
-            xmlHttpRequest.setRequestHeader('Content-Type', 'application/json');
-            xmlHttpRequest.send(JSON.stringify(propertyAccount));
-        }));
+    paymentInfoCollected : function(restLead) {
+        return !!restLead.propertyAccounts[0].zuoraPaymentRefId;
     },
 
-    finishApplication : function(restLead) {
+    savePaymentMethodToAccount : function(propertyAccount) {
+        return this.genericCallout(
+            '/apply/services/apexrest/v3/property-accounts',
+            'PATCH',
+            JSON.stringify(propertyAccount)
+        );
+    },
+
+    finishApplication : function(component) {
+        let restLead = JSON.stringify({
+            id: component.get('v.lead.id'),
+            email: component.get('v.lead.email'),
+            applicationCompleteDate: new Date(),
+            underwritingCriteria: component.get('v.lead.underwritingCriteria')
+        });
+        let callout = this.genericCallout(
+            '/apply/services/apexrest/v3/leads',
+            'PATCH',
+            restLead
+        ).then(
+            (resolveResult) => {
+                this.showFinalPage(component);
+            },
+            (rejectResult) => {
+                let message = 'Lead sent by PATCH to server: ' + restLead
+                              + '\n\n' + 'Callback received from server: ' + JSON.stringify(rejectResult);
+                this.logError(
+                    component,
+                    'SSFApplicationController',
+                    'helper.finishApplication',
+                    message
+                );
+                this.showFinalPage(component);
+            }
+        );
+    },
+
+    genericCallout : function(calloutURI, apiMethod, bodyAsString) {
         return new Promise($A.getCallback(function(resolve, reject) {
-            let calloutURI = '/apply/services/apexrest/v3/leads';
             const xmlHttpRequest = new XMLHttpRequest();
             xmlHttpRequest.onreadystatechange = function() {
                 if (this.readyState === 4) {
@@ -45,9 +57,9 @@
                     }
                 }
             };
-            xmlHttpRequest.open('PATCH', calloutURI, true);
+            xmlHttpRequest.open(apiMethod, calloutURI, true);
             xmlHttpRequest.setRequestHeader('Content-Type', 'application/json');
-            xmlHttpRequest.send(JSON.stringify(restLead));
+            xmlHttpRequest.send(bodyAsString);
         }));
     },
 
@@ -67,6 +79,5 @@
             "severity": "Error"
         });
         $A.enqueueAction(action);
-    }
-
+    },
 });
