0d894faa34ff3c193fa716bd68fa1af2a38a5c2b
Jeff Battles <jbattles@bluewavesolar.com>
2021-01-13 14:36:18 +0000

    US-0002042: W-021466: Update Customer Community

--- src/aura/CSPPaperlessUpdate/CSPPaperlessUpdateController.js
+++ src/aura/CSPPaperlessUpdate/CSPPaperlessUpdateController.js
@@ -4,36 +4,27 @@
         action.setCallback(this,function(resp){
             if(resp.getState() == 'SUCCESS') {
                 component.set("v.userContact", resp.getReturnValue());
-               
-            }
-        });
-        $A.enqueueAction(action);
-    }, 
-
-    tableEditable : function(component, event, helper) {
-        var currentState = component.get("v.isEditable");
-        component.set("v.isEditable", !currentState);
-    },
-
-    updateContact : function(component, event, helper) {
-        var updateAction = component.get("c.updateContactInfo");
-
-        updateAction.setParams({
-            "contactToUpdate": component.get("v.userContact")
-        });
-        
-        updateAction.setCallback(this,function(resp){
-            if(resp.getState() == 'SUCCESS') {
-                component.set("v.userContact", resp.getReturnValue());
-                component.set("v.isEditable", false);
+                var checkPaperless = component.get("c.isPaperless");
+                checkPaperless.setParams({
+                    "parentAccountId": resp.getReturnValue().AccountId
+                });
+                checkPaperless.setCallback(this,function(response){
+                    if(response.getState() == 'SUCCESS') {
+                        component.set("v.paperlessBill",response.getReturnValue());
+                    }
+                    else {
+                        $A.log("Errors", response.getError());
+                    }
+                });
+                $A.enqueueAction(checkPaperless);
             }
             else {
                 $A.log("Errors", resp.getError());
             }
         });
-        $A.enqueueAction(updateAction);
+        $A.enqueueAction(action);
     },
-
+   
     goPaperless : function(component, event, helper) {
         var setPropertiesToPaperless = component.get("c.setToPaperless");
         let contact = component.get("v.userContact");
